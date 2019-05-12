0437c4321c8c15032fe545c7ecf2774cb675732d
ZZMilkTEA <504267031@qq.com>
2019-05-12 15:42:24 +0800

    修复了获取canvas时id打错的错误...

--- colorTest/canvas.js
+++ colorTest/canvas.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("canvasTest");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 function draw() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
--- particalTest/canvas.js
+++ particalTest/canvas.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("canvasTest");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 var emmiter = (function () {
